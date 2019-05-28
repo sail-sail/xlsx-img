874718d88c625a7b0d62df5de4a068657c2dde6f
Sail <151263555@qq.com>
2019-05-28 19:39:18 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 const gulp = require("gulp");
 const ts = require('gulp-typescript');
 const uglify = require('gulp-uglify-es').default;
+const sourcemaps = require('gulp-sourcemaps');
 
 const project = "xlsx-img";
 
-const dist = `./dist/${project}`;
+const dist = `./build/${project}`;
 //----------------------------------------------------------------------------------------------------- config
 gulp.task("config", function() {
   return gulp.src(["package.json"], { allowEmpty: true })
@@ -14,11 +15,13 @@ gulp.task("config", function() {
 gulp.task("ts", function() {
   const tsProject = ts.createProject('tsconfig.json', { sourceMap: false });
   return tsProject.src()
+    .pipe(sourcemaps.init())
     .pipe(tsProject()).js
     .pipe(uglify({toplevel: false}))
     .on('error', function (err) {
       console.error(err);
     })
+    .pipe(sourcemaps.write(`./`))
     .pipe(gulp.dest(`${dist}/src`));
 });
 
