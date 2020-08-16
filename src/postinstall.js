68577fa32d0102d4706b7c2757e41cfb01a99ece
Sail <151263555@qq.com>
2020-08-17 01:06:00 +0800

    1.2.7

--- src/postinstall.js
+++ src/postinstall.js
@@ -5,11 +5,12 @@ function exec(file) {
     const stats = statSync(file);
     if(!stats.isFile()) return;
   } catch (err) {
+    // console.error(err);
     return;
   }
   let str = readFileSync(file, "utf8");
-  str = str.replace(".asNodeBuffer().toString('binary')", ".asNodeBuffer().toString()");
+  str = str.replace("return getdatastr(getzipfile(zip, file))", "return utf8read(getdatastr(getzipfile(zip, file)))");
   writeFileSync(file, str);
 }
-exec(`${ __dirname }/node_modules/xlsx/xlsx.js`);
+exec(`${ __dirname }/../node_modules/xlsx/xlsx.js`);
 exec(`${ __dirname }/../../xlsx/xlsx.js`);
